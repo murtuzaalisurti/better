9d5925b2332fa3717c13e890af2e5dc77a6a4ad5
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-08-17 19:12:51 +0530

    understanding position

--- index.js
+++ index.js
@@ -24,51 +24,8 @@ async function run() {
             core.info(JSON.stringify(pullRequest.data, null, 2));
             core.info(JSON.stringify(parsedDiff, null, 2));
 
-            // core.info(JSON.stringify(
-            //     parsedDiff.reduce((acc, file) => {
-            //         return acc.concat(file.chunks.reduce((accc, chunk) => {
-            //             return accc.concat(chunk.changes.filter(change => (change.type !== 'normal' && !change.content.includes("No newline at end of file"))).map((change, i, arr) => {
-            //                 if (change.type === 'add') {
-            //                     if (i > 0 && arr[i - 1].type === 'del' && change.ln === arr[i - 1].ln) {
-            //                         return {
-            //                             path: file.from,
-            //                             position: change.ln,
-            //                             body: `**${file.from}** added. This is a review comment.`,
-            //                             change,
-            //                             previously: arr[i - 1].content
-            //                         }
-            //                     }
-            //                     return {
-            //                         path: file.from,
-            //                         position: change.ln,
-            //                         body: `**${file.from}** added. This is a review comment.`,
-            //                         change
-            //                     }
-            //                 }
-
-            //                 if (change.type === 'del' && change.ln === arr[i + 1].ln && arr[i + 1].type === 'add') {
-            //                     return null
-            //                 }
-
-            //                 return {
-            //                     path: file.from,
-            //                     position: change.ln,
-            //                     body: `**${file.from}** changed to **${file.to}**. This is a review comment.`,
-            //                     change
-            //                 }
-            //             }).filter(i => i))
-            //         }, []))
-            //     }, []),
-            //     null, 2
-            // ))
-
-            await octokit.rest.pulls.createReview({
-                owner: github.context.repo.owner,
-                repo: github.context.repo.repo,
-                pull_number: github.context.payload.pull_request.number,
-                body: `Code Review by better`,
-                event: 'COMMENT',
-                comments: parsedDiff.reduce((acc, file) => {
+            core.info(JSON.stringify(
+                parsedDiff.reduce((acc, file) => {
                     return acc.concat(file.chunks.reduce((accc, chunk) => {
                         return accc.concat(chunk.changes.filter(change => (change.type !== 'normal' && !change.content.includes("No newline at end of file"))).map((change, i, arr) => {
                             if (change.type === 'add') {
@@ -76,7 +33,7 @@ async function run() {
                                     return {
                                         path: file.from,
                                         position: change.ln,
-                                        body: `**${arr[i - 1].content}** removed and **${change.content}** added. This is a review comment.`,
+                                        body: `**${file.from}** added. This is a review comment.`,
                                         change,
                                         previously: arr[i - 1].content
                                     }
@@ -84,7 +41,7 @@ async function run() {
                                 return {
                                     path: file.from,
                                     position: change.ln,
-                                    body: `**${change.content}** added. This is a review comment.`,
+                                    body: `**${file.from}** added. This is a review comment.`,
                                     change
                                 }
                             }
@@ -96,17 +53,60 @@ async function run() {
                             return {
                                 path: file.from,
                                 position: change.ln,
-                                body: `**${file.from}** modified to **${file.to}** and the content changed to **${change.content}**. This is a review comment.`,
+                                body: `**${file.from}** changed to **${file.to}**. This is a review comment.`,
                                 change
                             }
-                        }).filter(i => i).map(i => ({
-                            path: i.path,
-                            position: i.position,
-                            body: i.body
-                        })))
+                        }).filter(i => i))
                     }, []))
-                }, [])
-            })
+                }, []),
+                null, 2
+            ))
+
+            // await octokit.rest.pulls.createReview({
+            //     owner: github.context.repo.owner,
+            //     repo: github.context.repo.repo,
+            //     pull_number: github.context.payload.pull_request.number,
+            //     body: `Code Review by better`,
+            //     event: 'COMMENT',
+            //     comments: parsedDiff.reduce((acc, file) => {
+            //         return acc.concat(file.chunks.reduce((accc, chunk) => {
+            //             return accc.concat(chunk.changes.filter(change => (change.type !== 'normal' && !change.content.includes("No newline at end of file"))).map((change, i, arr) => {
+            //                 if (change.type === 'add') {
+            //                     if (i > 0 && arr[i - 1].type === 'del' && change.ln === arr[i - 1].ln) {
+            //                         return {
+            //                             path: file.from,
+            //                             position: change.ln,
+            //                             body: `**${arr[i - 1].content}** removed and **${change.content}** added. This is a review comment.`,
+            //                             change,
+            //                             previously: arr[i - 1].content
+            //                         }
+            //                     }
+            //                     return {
+            //                         path: file.from,
+            //                         position: change.ln,
+            //                         body: `**${change.content}** added. This is a review comment.`,
+            //                         change
+            //                     }
+            //                 }
+
+            //                 if (change.type === 'del' && change.ln === arr[i + 1].ln && arr[i + 1].type === 'add') {
+            //                     return null
+            //                 }
+
+            //                 return {
+            //                     path: file.from,
+            //                     position: change.ln,
+            //                     body: `**${file.from}** modified to **${file.to}** and the content changed to **${change.content}**. This is a review comment.`,
+            //                     change
+            //                 }
+            //             }).filter(i => i).map(i => ({
+            //                 path: i.path,
+            //                 position: i.position,
+            //                 body: i.body
+            //             })))
+            //         }, []))
+            //     }, [])
+            // })
         }
         const time = (new Date()).toTimeString();
         core.info(`${time}, ${JSON.stringify(github.context.repo, null, 2)}, ${stargazers.data}`);
