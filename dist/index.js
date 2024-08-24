2a4917aad5de66c8403177cde18dc50df83ff55d
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-08-24 11:58:00 +0530

    refactor: test multiple add lines

--- dist/index.js
+++ dist/index.js
@@ -37056,7 +37056,7 @@ __nccwpck_require__.d(error_namespaceObject, {
 });
 
 // EXTERNAL MODULE: ./node_modules/@actions/core/lib/core.js
-var lib_core = __nccwpck_require__(2186);
+var core = __nccwpck_require__(2186);
 // EXTERNAL MODULE: ./node_modules/@actions/github/lib/github.js
 var lib_github = __nccwpck_require__(5438);
 // EXTERNAL MODULE: ./node_modules/parse-diff/index.js
@@ -47141,6 +47141,7 @@ function getCommentsToAdd(parsedDiff) {
                              */
                             if (arr[i - 1].type === 'add' && change.ln === arr[i - 1].ln + 1 && arr[i - 1].content !== "+") {
                                 // might want to remove this check to be able to feed the data to AI
+                                arr[i - 1].content += change.content;
                                 return null
                             }
 
@@ -47218,90 +47219,90 @@ async function addReviewComments(parsedDiff, octokit) {
 }
 async function run() {
     try {
-        const token = lib_core.getInput('repo-token');
-        const modelToken = lib_core.getInput('ai-model-api-key');
+        const token = core.getInput('repo-token');
+        const modelToken = core.getInput('ai-model-api-key');
         const octokit = lib_github.getOctokit(token);
 
         const openAI = new openai({
             apiKey: modelToken
         })
 
-        console.log('calling openAI chat.completions.create')
-        const aiResult = await openAI.chat.completions.create({
-            model: 'gpt-4o-mini-2024-07-18',
-            messages: [
-                {
-                    role: 'system',
-                    content: 'I want you to act as a code reviewer. I will provide you with a diff payload and I want you to make suggestions on what can be improved by looking at the diff changes. Keep the suggestions precise and to the point (in a constructive way).',
-                },
-                {
-                    role: 'user',
-                    content: `Now what I want you to do is, take this diff payload and analyze the changes from the "content" and "previously" properties of the payload and suggest some improvements. If you think there are no improvements to be made, don't return **that** object from the payload. Rest, **return everything as it is (in the same order)** along with your suggestions. And, return the response as a json array of objects (not json markdown format). Here's the diff:
-                    [
-  {
-    "path": "index.js",
-    "position": 3,
-    "line": 1,
-    "body": "**index.js** added. This is a review comment.",
-    "change": {
-      "type": "add",
-      "add": true,
-      "ln": 1,
-      "content": "+console.log(\"Hey there, World! ----------\");",
-      "relativePosition": 3
-    },
-    "previously": "-console.log(\"Hello, World!\");"
-  },
-  {
-    "path": "index.js",
-    "position": 5,
-    "line": 3,
-    "body": "**index.js** added. This is a review comment.",
-    "change": {
-      "type": "add",
-      "add": true,
-      "ln": 3,
-      "content": "+(() => {",
-      "relativePosition": 5
-    }
-  },
-  {
-    "path": "package.json",
-    "position": 4,
-    "line": 3,
-    "body": "**package.json** added. This is a review comment.",
-    "change": {
-      "type": "add",
-      "add": true,
-      "ln": 3,
-      "content": "+  \"version\": \"1.1.2\",",
-      "relativePosition": 4
-    },
-    "previously": "-  \"version\": \"1.0.0\","
-  },
-  {
-    "path": "package.json",
-    "position": 7,
-    "line": 6,
-    "body": "**package.json** added. This is a review comment.",
-    "change": {
-      "type": "add",
-      "add": true,
-      "ln": 6,
-      "content": "+    \"start\": \"node index.js\",",
-      "relativePosition": 7
-    }
-  }
-]`
-                }
-            ],
-            // response_format: zodResponseFormat(diffPayloadSchema, 'json_diff_response')
-        })
+        // console.log('calling openAI chat.completions.create')
+//         const aiResult = await openAI.chat.completions.create({
+//             model: 'gpt-4o-mini-2024-07-18',
+//             messages: [
+//                 {
+//                     role: 'system',
+//                     content: 'I want you to act as a code reviewer. I will provide you with a diff payload and I want you to make suggestions on what can be improved by looking at the diff changes. Keep the suggestions precise and to the point (in a constructive way).',
+//                 },
+//                 {
+//                     role: 'user',
+//                     content: `Now what I want you to do is, take this diff payload and analyze the changes from the "content" and "previously" properties of the payload and suggest some improvements. If you think there are no improvements to be made, don't return **that** object from the payload. Rest, **return everything as it is (in the same order)** along with your suggestions. And, return the response as a json array of objects (not json markdown format). Here's the diff:
+//                     [
+//   {
+//     "path": "index.js",
+//     "position": 3,
+//     "line": 1,
+//     "body": "**index.js** added. This is a review comment.",
+//     "change": {
+//       "type": "add",
+//       "add": true,
+//       "ln": 1,
+//       "content": "+console.log(\"Hey there, World! ----------\");",
+//       "relativePosition": 3
+//     },
+//     "previously": "-console.log(\"Hello, World!\");"
+//   },
+//   {
+//     "path": "index.js",
+//     "position": 5,
+//     "line": 3,
+//     "body": "**index.js** added. This is a review comment.",
+//     "change": {
+//       "type": "add",
+//       "add": true,
+//       "ln": 3,
+//       "content": "+(() => {",
+//       "relativePosition": 5
+//     }
+//   },
+//   {
+//     "path": "package.json",
+//     "position": 4,
+//     "line": 3,
+//     "body": "**package.json** added. This is a review comment.",
+//     "change": {
+//       "type": "add",
+//       "add": true,
+//       "ln": 3,
+//       "content": "+  \"version\": \"1.1.2\",",
+//       "relativePosition": 4
+//     },
+//     "previously": "-  \"version\": \"1.0.0\","
+//   },
+//   {
+//     "path": "package.json",
+//     "position": 7,
+//     "line": 6,
+//     "body": "**package.json** added. This is a review comment.",
+//     "change": {
+//       "type": "add",
+//       "add": true,
+//       "ln": 6,
+//       "content": "+    \"start\": \"node index.js\",",
+//       "relativePosition": 7
+//     }
+//   }
+// ]`
+//                 }
+//             ],
+//             // response_format: zodResponseFormat(diffPayloadSchema, 'json_diff_response')
+//         })
 
-        console.log(JSON.stringify(JSON.parse(aiResult.choices[0].message.content), null, 2))
+        // console.log(JSON.stringify(JSON.parse(aiResult.choices[0].message.content), null, 2))
 
         if (lib_github.context.payload.pull_request) {
-            lib_core.info('Reviewing pull request...');
+            core.info('Reviewing pull request...');
 
             const pullRequest = await octokit.rest.pulls.get({
                 owner: lib_github.context.repo.owner,
@@ -47314,12 +47315,12 @@ async function run() {
 
             const parsedDiff = parse_diff(pullRequest.data);
 
-            // getCommentsToAdd(parsedDiff).printJSON();
+            getCommentsToAdd(parsedDiff).printJSON();
             // await addReviewComments(parsedDiff, octokit);
         }
     } catch (error) {
-        lib_core.error(error);
-        lib_core.setFailed(error.message);
+        core.error(error);
+        core.setFailed(error.message);
     }
 }
 
