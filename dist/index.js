cbd1bb65c07bfc7ad23b8f3be4cfc2eeaaa8a1bc
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-08-24 14:43:56 +0530

    feat: typesafe

--- dist/index.js
+++ dist/index.js
@@ -48534,37 +48534,42 @@ function zodFunction(options) {
 
 
 
-/**
- * prompt
- * now what I want you to do is, take this diff payload and analyze the changes from the "content" and "previously" properties of the payload and suggest some improvements. If you think there are no improvements to be made, don't return such object from the payload. Rest, return everything as it is (in the same order) along with your suggestions.
- */
+const aDiff = z.object({
+    path: z.string(),
+    position: z.number(),
+    line: z.number(),
+    body: z.string(),
+    change: z.object({
+        type: z.string(),
+        add: z.boolean(),
+        ln: z.number(),
+        content: z.string(),
+        relativePosition: z.number(),
+    }),
+    previously: z.string().optional(),
+    suggestions: z.string().optional(),
+});
 
 const diffPayloadSchema = z.object(
     {
-        commentsToAdd: z.array(
-            z.object({
-                path: z.string(),
-                position: z.number(),
-                line: z.number(),
-                body: z.string(),
-                change: z.object({
-                    type: z.string(),
-                    add: z.boolean(),
-                    ln: z.number(),
-                    content: z.string(),
-                    relativePosition: z.number(),
-                }),
-                previously: z.string().optional(),
-                suggestions: z.string().optional(),
-            })
-        )
+        commentsToAdd: z.array(aDiff)
     }
 );
+
+/**
+ * @typedef {z.infer<typeof aDiff>[]} rawCommentsPayload
+ * @typedef {z.infer<typeof diffPayloadSchema>} suggestionsPayload
+ * @typedef {{ path: string, line: number, body: string }[]} CommentsPayload
+ */
+
 /**
  * 
  * @param {parseDiff.File[]} parsedDiff 
  */
 function getCommentsToAdd(parsedDiff) {
+    /**
+     * @returns {rawCommentsPayload}
+     */
     const comments = () => parsedDiff.reduce((acc, file) => {
         let diffRelativePosition = 0;
         return acc.concat(file.chunks.reduce((accc, chunk, i) => {
@@ -48624,11 +48629,11 @@ function getCommentsToAdd(parsedDiff) {
     }, [])
 
     /**
-     * @param {Record<string, string>[]} rawComments
+     * @typedef
+     * @param {rawCommentsPayload} rawComments
      * @param {OpenAI} openAI 
      */
     const getSuggestions = async (rawComments, openAI) => {
-        console.log(JSON.stringify(rawComments, null, 2))
         const result = await openAI.beta.chat.completions.parse({
             model: 'gpt-4o-mini-2024-07-18',
             messages: [
@@ -48653,17 +48658,12 @@ function getCommentsToAdd(parsedDiff) {
         return result.choices[0].message.parsed
     }
 
-    function printJSON() {
-        core.info(JSON.stringify(comments(), null, 2))
-    }
-
     return {
         raw: comments,
         getSuggestions,
-        printJSON,
         /**
-         * @param {diffPayloadSchema} suggestions
-         * @returns {{ path: string, line: number, body: string }[]}
+         * @param {suggestionsPayload} suggestions
+         * @returns {CommentsPayload}
          */
         comments: (suggestions) => suggestions.commentsToAdd.filter(i => {
             return i["suggestions"]
@@ -48705,76 +48705,6 @@ async function run() {
         })
 
         console.log('calling openAI chat.completions.create')
-//         const aiResult = await openAI.beta.chat.completions.parse({
-//             model: 'gpt-4o-mini-2024-07-18',
-//             messages: [
-//                 {
-//                     role: 'system',
-//                     content: 'I want you to act as a code reviewer. I will provide you with a diff payload and I want you to make suggestions on what can be improved by looking at the diff changes. Keep the suggestions precise and to the point (in a constructive way).',
-//                 },
-//                 {
-//                     role: 'user',
-//                     content: `Now what I want you to do is, take this diff payload and analyze the changes from the "content" and "previously" properties of the payload and suggest some improvements. If you think there are no improvements to be made, don't return **that** object from the payload. Rest, **return everything as it is (in the same order)** along with your suggestions. And, return the response as a json array of objects (not json markdown format). Here's the diff:
-//                     [
-//   {
-//     "path": "index.js",
-//     "position": 3,
-//     "line": 1,
-//     "body": "**index.js** added. This is a review comment.",
-//     "change": {
-//       "type": "add",
-//       "add": true,
-//       "ln": 1,
-//       "content": "+console.log(\"Hey there, World! ----------\");",
-//       "relativePosition": 3
-//     },
-//     "previously": "-console.log(\"Hello, World!\");"
-//   },
-//   {
-//     "path": "index.js",
-//     "position": 5,
-//     "line": 3,
-//     "body": "**index.js** added. This is a review comment.",
-//     "change": {
-//       "type": "add",
-//       "add": true,
-//       "ln": 3,
-//       "content": "+(() => {",
-//       "relativePosition": 5
-//     }
-//   },
-//   {
-//     "path": "package.json",
-//     "position": 4,
-//     "line": 3,
-//     "body": "**package.json** added. This is a review comment.",
-//     "change": {
-//       "type": "add",
-//       "add": true,
-//       "ln": 3,
-//       "content": "+  \"version\": \"1.1.2\",",
-//       "relativePosition": 4
-//     },
-//     "previously": "-  \"version\": \"1.0.0\","
-//   },
-//   {
-//     "path": "package.json",
-//     "position": 7,
-//     "line": 6,
-//     "body": "**package.json** added. This is a review comment.",
-//     "change": {
-//       "type": "add",
-//       "add": true,
-//       "ln": 6,
-//       "content": "+    \"start\": \"node index.js\",",
-//       "relativePosition": 7
-//     }
-//   }
-// ]`
-//                 }
-//             ],
-//             response_format: zodResponseFormat(diffPayloadSchema, 'json_diff_response')
-//         })
 
         if (github.context.payload.pull_request) {
             core.info('Reviewing pull request...');
