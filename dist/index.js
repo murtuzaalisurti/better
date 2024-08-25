dfeb42b031a497ec82e958128c6c5ed0fbcb8bc7
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-08-25 16:37:21 +0530

    try del review comms

--- dist/index.js
+++ dist/index.js
@@ -48748,16 +48748,17 @@ async function run() {
                     review_id: reviewsByBot[0].id, 
                 })
 
-                await octokit.rest.pulls.dismissReview({
-                    owner: github.context.repo.owner,
-                    repo: github.context.repo.repo,
-                    pull_number: github.context.payload.pull_request.number,
-                    review_id: reviewsByBot[0].id,
-                    message: "Outdated",
-                    event: "DISMISS"
-                })
-
                 console.log(JSON.stringify(reviewComments, null, 2))
+
+                for (const comment of reviewComments.data) {
+                    await octokit.rest.pulls.deleteReviewComment({
+                        owner: github.context.repo.owner,
+                        repo: github.context.repo.repo,
+                        comment_id: comment.id,
+                    })
+                    await new Promise(resolve => setTimeout(resolve, 1500)) // Wait 1.5 seconds
+                }
+
                 // return;
             }
 
