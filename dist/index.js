0639507aab5c704bc1a01f3a713aaec187ea70da
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-08-27 22:56:51 +0530

    fix: improve prompt

--- dist/index.js
+++ dist/index.js
@@ -48652,8 +48652,8 @@ function getCommentsToAdd(parsedDiff) {
                     - If something is deleted (type: "del"), compare it with what's added (type: "add") in place of it. If it's completely different, ignore the deleted part and give suggestions based on the added (type: "add") part.
                     - Only modify/add the "suggestions" property (if required).
                     - DO NOT modify the value of any other property. Return them as they are in the input.
-                    - Make sure the suggestion positions are accurate as they are in the input and suggestions are related to the code changes.
-                    - If there is a suggestion which is similar across multiple lines, only suggest that change at any one of the lines.
+                    - Make sure the suggestion positions are accurate as they are in the input and suggestions are related to the code changes on those positions.
+                    - If there is a suggestion which is similar across multiple positions, only suggest that change at any one of those positions.
                     - Keep the suggestions precise and to the point (in a constructive way).
                     - If possible, add references to some really good resources like stackoverflow or from programming articles, blogs, etc. for suggested code changes. Keep the references in context of the programming language you are reviewing.
                     - Suggestions should be inclusive of the rules (if any) provided by the user.
