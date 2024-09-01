b4eca169ca7f5d75162036632228b9e539f5f966
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-09-01 14:19:59 +0530

    don't be lazy

--- dist/index.js
+++ dist/index.js
@@ -51253,6 +51253,7 @@ async function getSuggestions(rawComments, openAI, rules, modelName, pullRequest
                 If you think there are no improvements to be made, don't return **that** object from the payload.
                 Rest, **return everything as it is (in the same order)** along with your suggestions. Ignore formatting issues.
                 IMPORTANT: 
+                - Don't be lazy.
                 - Only make suggestions when they are significant, relevant and add value to the code changes.
                 - If something is deleted (type: "del"), compare it with what's added (type: "add") in place of it. If it's completely different, ignore the deleted part and give suggestions based on the added (type: "add") part.
                 - Only modify/add the "suggestions" property (if required).
