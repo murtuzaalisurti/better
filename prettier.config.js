2792c9997bed979d60d37c9ba77405729b450794
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-09-03 22:25:05 +0530

    feat: configure prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    printWidth: 120,
+};
+
+export default config;
