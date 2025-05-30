568f99551597fa7a91bd7b25b7cced36ea2ef0a1
murtuzaalisurti <murtuza.creativity@gmail.com>
2025-05-30 22:55:51 +0530

    fix: increase review retrieval size

--- index.js
+++ index.js
@@ -540,6 +540,7 @@ async function getAllReviewsForPullRequest(octokit) {
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         pull_number: github.context.payload.pull_request.number,
+        per_page: 500,
     });
 }
 
